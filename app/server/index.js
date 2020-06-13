8cfb6b14d211b38ac75752d784a3219d7c5fc8b8
silvanoamaral <silvano.amaral9@gmail.com>
2020-06-12 22:16:48 -0300

    Update index.js

--- app/server/index.js
+++ app/server/index.js
@@ -8,10 +8,10 @@ const { consolidado } = require('../modules/consolidado')
 app.use('/api/consolidado', consolidado)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../build')))
+  app.use(express.static('dist'))
   
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../../build/index.html'))
+    res.sendFile(path.resolve(__dirname, 'dist','index.html'))
   })
 }
 
